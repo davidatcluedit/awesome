9599d4f21918d46239499e3133eb404ba4d783e7
sgkim <qtudio@gmail.com>
2018-12-06 15:04:30 +0900

    update description

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,18 +1,18 @@
 module.exports = {
-    title: 'Scratch',
-    description: 'Javascript & Typescript',
-    themeConfig: {
-        nav: [
-            { text: 'Home', link: '/' },
-            { text: 'Javascirpt', link: '/javascript/' },
-            { text: 'Typescript', link: '/typescript/' },
-            { text: 'Docker', link: '/docker/' },
-            { text: 'Wiki', link: 'https://github.com/cluedit/cluedit.github.io/wiki' },
-            { text: 'Github', link: 'https://github.com/cluedit' },
-        ],
-        sidebar: 'auto',
-        serviceWorker: {
-            updatePopup: true,
-        },
+  title: 'Scratch',
+  description: 'From Scratch',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Javascirpt', link: '/javascript/' },
+      { text: 'Typescript', link: '/typescript/' },
+      { text: 'Docker', link: '/docker/' },
+      { text: 'Wiki', link: 'https://github.com/cluedit/cluedit.github.io/wiki' },
+      { text: 'Github', link: 'https://github.com/cluedit' },
+    ],
+    sidebar: 'auto',
+    serviceWorker: {
+      updatePopup: true,
     },
+  },
 }
